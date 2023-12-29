8c40c9431d30dc0db306416d9ac9bc16fef1157f
Сергей Шевчук <you@example.com>
2023-12-29 10:16:36 +0200

    burger

--- JS/mobile-menu.js
+++ JS/mobile-menu.js
@@ -2,8 +2,23 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const menuBtnOpen = document.querySelector(".menu-btn-open");
 const menuBtnClose = document.querySelector(".menu-btn-close");
+const formatsSectionSlider = document.querySelector(".formats-section__slider");
 
-const toggleMenu = () => mobileMenu.classList.toggle("is-open");
+const toggleMenu = () => {
+  mobileMenu.classList.toggle("is-open");
+  formatsSectionSlider.classList.toggle("is-hidden");
+  document.body.classList.toggle("is-mobile-menu-open"); // добавьте эту строку
+};
 
 menuBtnOpen.addEventListener("click", toggleMenu);
 menuBtnClose.addEventListener("click", toggleMenu);
+
+const mobileNavLinks = document.querySelectorAll(".mobile-menu__nav a");
+
+mobileNavLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("is-open");
+    formatsSectionSlider.classList.remove("is-hidden");
+    document.body.classList.remove("is-mobile-menu-open"); // добавьте эту строку
+  });
+});
